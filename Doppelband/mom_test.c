f15fe2a8a4dc4baed06ea4a9e80892b0fc5789f8
wu <wuchuanren@gmail.com>
2012-04-22 12:06:39 +0200

    generate plot files for mom_test 	modified:   Makefile 	modified:   mom_test.c

--- Doppelband/mom_test.c
+++ Doppelband/mom_test.c
@@ -26,10 +26,12 @@ void test_mesh()
 void test_matrix()
 {
 	struct MeshConfig *conf;
-	struct MD *x[2];
+	struct MD *x[2], *x0[2];
 	int i;
 	double c[2];
 	double z0;
+
+	FILE *fplot;
 
 	conf = mesh_new(
 		2.4e-3, 10e-3,
@@ -39,71 +41,99 @@ void test_matrix()
 		2.2
 		);
 
-	z0 = mom(conf, NULL, x, c);
+	z0 = mom(conf, x0, x, c);
+
 
 	printf("C0 = %le F        C1 = %le F\n", c[0], c[1]);
 	printf("Z0 = %lf Ohm\n", z0);
 
+	fplot = fopen("dielectric.plot", "w");
 
-	printf("#!"PATH_GNUPLOT"\n");
-	printf("# top all charges\n");
-	printf("plot '-' notitle with impulse\n");
+	if (fplot) {
+		fprintf(fplot, "#!"PATH_GNUPLOT"\n");
+		fprintf(fplot, "# top all charges\n");
+		fprintf(fplot, "plot '-' notitle with impulse, \\\n"
+				"\t'-' notitle with impulse\n");
 
-	for (i = conf->index[ID_STRIP0_START];
-		i < conf->index[ID_STRIP0_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+		for (i = conf->index[ID_STRIP0_START];
+			i < conf->index[ID_STRIP0_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+		}
+
+		for (i = conf->index[ID_DIELECTRIC0_START];
+			i < conf->index[ID_DIELECTRIC0_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+		}
+		fprintf(fplot, "e\n");
+
+		fprintf(fplot, "# bottom all charges\n");
+
+		for (i = conf->index[ID_STRIP1_START];
+			i < conf->index[ID_STRIP1_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+		}
+
+		for (i = conf->index[ID_DIELECTRIC1_START];
+			i < conf->index[ID_DIELECTRIC1_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+		}
+		fprintf(fplot, "e\npause -1\n");
+		/*****/
+		fprintf(fplot, "# top free charges\n");
+		fprintf(fplot, "plot '-' notitle with impulse, \\\n"
+				"\t'-' notitle with impulse\n");
+
+		for (i = conf->index[ID_STRIP0_START];
+			i < conf->index[ID_STRIP0_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
+		}
+
+		for (i = conf->index[ID_DIELECTRIC0_START];
+			i < conf->index[ID_DIELECTRIC0_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
+		}
+		fprintf(fplot, "e\n");
+
+		fprintf(fplot, "# bottom free charges\n");
+
+		for (i = conf->index[ID_STRIP1_START];
+			i < conf->index[ID_STRIP1_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
+		}
+
+		for (i = conf->index[ID_DIELECTRIC1_START];
+			i < conf->index[ID_DIELECTRIC1_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
+		}
+		fprintf(fplot, "e\npause -1\n");
+		fclose(fplot);
 	}
 
-	for (i = conf->index[ID_DIELECTRIC0_START];
-		i < conf->index[ID_DIELECTRIC0_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+	fplot = fopen("freespace.plot", "w");
+
+	if (fplot) {
+		fprintf(fplot, "#!"PATH_GNUPLOT"\n");
+		fprintf(fplot, "# top free charges\n");
+		fprintf(fplot, "plot '-' notitle with impulse, \\\n"
+				"\t'-' notitle with impulse\n");
+
+		for (i = conf->index[ID_STRIP0_START];
+			i < conf->index[ID_STRIP0_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+		}
+
+		fprintf(fplot, "e\n");
+
+		fprintf(fplot, "# bottom free charges\n");
+
+		for (i = conf->index[ID_STRIP1_START];
+			i < conf->index[ID_STRIP1_END]; ++i) {
+			fprintf(fplot, "%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
+		}
+
+		fprintf(fplot, "e\npause -1\n");
+		fclose(fplot);
 	}
-	printf("e\npause -1\n");
-
-	printf("# bottom all charges\n");
-	printf("plot '-' notitle with impulse\n");
-
-	for (i = conf->index[ID_STRIP1_START];
-		i < conf->index[ID_STRIP1_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
-	}
-
-	for (i = conf->index[ID_DIELECTRIC1_START];
-		i < conf->index[ID_DIELECTRIC1_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[1]->buf[i]);
-	}
-	printf("e\npause -1\n");
-
-	printf("# top free charges\n");
-	printf("plot '-' notitle with impulse\n");
-
-	for (i = conf->index[ID_STRIP0_START];
-		i < conf->index[ID_STRIP0_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
-	}
-
-	for (i = conf->index[ID_DIELECTRIC0_START];
-		i < conf->index[ID_DIELECTRIC0_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
-	}
-	printf("e\npause -1\n");
-
-	printf("# bottom free charges\n");
-	printf("plot '-' notitle with impulse\n");
-
-	for (i = conf->index[ID_STRIP1_START];
-		i < conf->index[ID_STRIP1_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
-	}
-
-	for (i = conf->index[ID_DIELECTRIC1_START];
-		i < conf->index[ID_DIELECTRIC1_END]; ++i) {
-		printf("%le %le\n", conf->mesh[i].centre, x[0]->buf[i]);
-	}
-	printf("e\npause -1\n");
-
-
-
 	md_free(x[0]);
 	md_free(x[1]);
 
