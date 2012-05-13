7a6d15276e531ba50ae2b2a76f2899ee690489b5
wu <wuchuanren@gmail.com>
2012-05-13 20:34:57 +0200

    modified:   build-mingw.bat 	modified:   ui/Makefile 	new file:   ui/win32_manifest.rc

--- Doppelband/src/build-mingw.bat
+++ Doppelband/src/build-mingw.bat
@@ -6,6 +6,7 @@ set CFLAGS=-O3 -o%BuildDir%\\%ExeName% -DUNICODE -DMOM_ENABLE_COMCTL -D_WIN32_IE
 mkdir %BuildDir%
 
 windres -o "%BuildDir%\resource.o" ui\win32.rc
+windres -o "%BuildDir%\manifest.o" ui\win32_manifest.rc
 
-gcc %CFLAGS% -mwindows mom.c mesh.c md.c global.c ui\win32.c %BuildDir%\resource.o -lcomctl32 -lgdi32
+gcc %CFLAGS% -mwindows mom.c mesh.c md.c global.c ui\win32.c %BuildDir%\resource.o %BuildDir%\manifest.o -lcomctl32 -lgdi32
 strip %BuildDir%\\%ExeName%
